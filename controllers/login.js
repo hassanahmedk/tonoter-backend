9f1de7d419b23190b72eb4f2a30ab87fed925b5f
Hassan <mr.und3ad1@gmail.com>
2022-11-28 01:54:25 +0500

    added cors headers in login controller

--- controllers/login.js
+++ controllers/login.js
@@ -2,44 +2,17 @@ import userModel from "../db/users.js";
 
 export const authorizeLogin = async (req, res) => {
     try{
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
+     
         userModel.findById(req.body.username)
         .then((user)=>{
             console.log(user);
           if(user === null){
-            res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
-      res.json({response:"User Not Found", loginAttempt:false})
+                res.json({response:"User Not Found", loginAttempt:false})
           } else {
             if(user.password === req.body.password){
-                res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
-      res.json({response:"Logged In", loginAttempt:true})
+                res.json({response:"Logged In", loginAttempt:true})
             } else {
-                res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
-      res.json({response:"Password Incorrect", loginAttempt:false})
+                res.json({response:"Password Incorrect", loginAttempt:false})
             }
           }
 
