77e349659e4ec52da264a20938cfdb258790cf15
Hassan <mr.und3ad1@gmail.com>
2022-11-29 17:05:00 +0500

    cors updated

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.urlencoded({extended:true}));
