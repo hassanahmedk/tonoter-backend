1cb4b178f348a3059905e9d0d73816bf1f34b506
Hassan <mr.und3ad1@gmail.com>
2023-03-08 09:23:20 +0500

    added id attribute to list items

--- db/lists.js
+++ db/lists.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
-
+import { nanoid } from "nanoid";
 
 // CREATE USER HERE
 
 const ItemSchema = new mongoose.Schema({
-    data: String,
-    important: Boolean
+  _id: {
+    type: String,
+    default: () => nanoid(),
+  },
+  data: String,
+  important: Boolean,
 });
 
 export const ListsSchema = new mongoose.Schema({
-    name: String,
-    items: [ItemSchema]
+  name: String,
+  items: [ItemSchema],
 });
-
 
 // const user = mongoose.model('User', userSchema);
 
 // const item = mongoose.model('Item', itemSchema);
 
-
-
 // const ListModel = mongoose.model("Notes", ListSchema);
-  
-// export default ListModel;
 
+// export default ListModel;
