fff58c36541858e9073baad28aaac5310c59c230
Hassan Ahmed Khan <89640679+mrund3ad1@users.noreply.github.com>
2023-03-09 18:39:11 +0500

    Update lists.js

--- db/lists.js
+++ db/lists.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import { nanoid } from "nanoid";
+
 
 // CREATE USER HERE
 
 const ItemSchema = new mongoose.Schema({
   uniqueId: {
     type: String,
-    default: new mongoose.Types.ObjectId()
   },
   data: String,
   important: Boolean,
